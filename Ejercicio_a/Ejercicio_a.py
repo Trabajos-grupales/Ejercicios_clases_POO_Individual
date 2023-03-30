e2746ed6af9b54c0af441c117ad5005033c3e339
luciacantos <luciacantos22@gmail.com>
2023-03-31 00:14:53 +0200

    clase Palíndromo

---
+++ Ejercicio_a/Ejercicio_a.py
@@ -0,0 +1,23 @@
+''''''''''''''''
+a. Palíndromo - método de clase
+Enunciado: crear una clase Palindromo que contenga un método de clase esPalindromo(), que devuelve un valor booleano que indica si una cadena de caracteres pasada como argumento es un palíndromo. Un palíndromo es una cadena que se puede leer de izquierda a derecha o de derecha a izquierda. Se tienen en cuenta los caracteres no alfanuméricos.
+
+Comportamiento esperado:
+
+print(Palindromo.esPalindromo('radar'))
+>>> True
+print(Palindromo.esPalindromo('sonar'))
+>>> False
+print(Palindromo.esPalindromo('Arde ya la yedra'))
+>>> False
+print(Palindromo.esPalindromo('Ardeyalayedra'))
+>>> True
+print(Palindromo.esPalindromo('!@#$% %$#@!'))
+>>> True
+print(Palindromo.esPalindromo('L O L'))
+>>> True
+'''''''''''''''''
+
+class Palindromo:
+
+    
